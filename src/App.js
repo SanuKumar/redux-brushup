c6a4691f7152775729c353637618b02b463b4406
SanuKumar <sanukumar1138@gmail.com>
2022-05-24 16:00:46 +0530

    simple redux setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { userAction } from "./index";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Users
+      <div>
+        <button onClick={() => dispatch(userAction())}>Get Users</button>
+      </div>
+      <ol>
+        {userData.length && userData.map((u) => <li key={u.id}>{u.name}</li>)}
+      </ol>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const initialState = {};
+
+const middleware = [thunk];
+
+const userListReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "USER":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const userAction = () => async (dispatch) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  dispatch({
+    type: "USER",
+    payload: data,
+  });
+};
+
+const reducer = combineReducers({
+  userList: userListReducer,
+});
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
